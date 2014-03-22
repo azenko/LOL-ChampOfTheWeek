7221f86fe989765d758f5458ad04bc06e69b1992
azenko <postmaster@leguidedelinternaute.com>
2014-03-22 01:50:22 +0100

    Function Separation & Cache System
    Cut OldCode To Get Function Code.
    Added a Plugin File Cache System and Wordpress Cache Detection for using
    the wordpress cache if enabled.

--- LOL-ChampOfTheWeek.php
+++ LOL-ChampOfTheWeek.php
@@ -23,45 +23,124 @@
         along with this program; if not, write to the Free Software
         Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     */
+    $COTW_API_KEY = "0b90ba9e-4838-43b6-9e7a-435c5bc333ef";                                                                                                     // API KEY
+    $COTW_API_REGION = "euw";                                                                                                                                   // Server REGION
+    $COTW_API_URL = "https://prod.api.pvp.net/";                                                                                                                // API URI
+    $COTW_SPRITEDIR_URI = "http://ddragon.leagueoflegends.com/cdn/4.4.3/img/sprite/";                                                                           // URI of the Sprite DIR of LoL Champ Icon
+        
+    $COTW_CHAMPLIST_URL = "http://prod.api.pvp.net/api/lol/static-data/".$COTW_API_REGION."/v1/champion?locale=fr_FR&champData=image&api_key=".$COTW_API_KEY;   // API URI GET CHAMPIONS LIST
+    $COTW_CHAMPWEEK_URL = "http://prod.api.pvp.net/api/lol/".$COTW_API_REGION."/v1.1/champion?freeToPlay=true&api_key=".$COTW_API_KEY;                          // API URI GET CHAMPIONS OF THE WEEK
 
-    $COTW_API_KEY = "0b90ba9e-4838-43b6-9e7a-435c5bc333ef";
-    $COTW_API_REGION = "euw";
-    $COTW_API_URL = "https://prod.api.pvp.net/";
-    $COTW_SPRITEDIR_URI = "http://ddragon.leagueoflegends.com/cdn/4.4.3/img/sprite/"; // URI of the Sprite DIR of LoL Champ Icon
+    $COTW_CHAMPLIST = "";
+    $COTW_CHAMPWEEK = "";
+
+    $COTW_CACHE = plugin_dir_path( __FILE__ ) . "cache/COTW.tmp";
+
+    function COTW_GetChampionsList()
+    {
+        global $COTW_CHAMPLIST_URL;
+
+        // Get Champions List
+        // API REQUEST
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$COTW_CHAMPLIST_URL);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        $returned = curl_exec($ch);
+         curl_close ($ch);
+        // API JSON DECODE
+        $chlist = json_decode($returned);
+        $chlist = $chlist->data;
+
+        // Return Champions Array
+        return $chlist;
+    }
     
-    $COTW_CHAMPLIST_URL = "http://prod.api.pvp.net/api/lol/static-data/".$COTW_API_REGION."/v1/champion?locale=fr_FR&champData=image&api_key=".$COTW_API_KEY;
-    $COTW_CHAMPWEEK_URL = "http://prod.api.pvp.net/api/lol/".$COTW_API_REGION."/v1.1/champion?freeToPlay=true&api_key=".$COTW_API_KEY;
-    
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$COTW_CHAMPLIST_URL);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_POST, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    $returned = curl_exec($ch);
-     curl_close ($ch);
+    function COTW_CacheEnabled()
+    {
+        wp_cache_set( 'COTW_CACHEORNOTCACHE', "CacheWork?", "COTW", 60);
+        $COTW_CACHETEST = wp_cache_get('COTW_CACHEORNOTCACHE');
+        if ( false === $COTW_CACHETEST ) {
+            return false;
+        }
+        return true; 
+    }
 
-    $chlist = json_decode($returned);
-    $chlist = $chlist->data;
+    function COTW_GetChampionsOfTheWeek()
+    {
+        global $COTW_CHAMPWEEK_URL;
+        if(COTW_CacheEnabled())
+        {
+            $result = wp_cache_get('COWT_CHAMPOFWEEK');
+            if ( false === $result ) {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,$COTW_CHAMPWEEK_URL);
+                curl_setopt($ch, CURLOPT_VERBOSE, 1);
+                curl_setopt($ch, CURLOPT_POST, 0);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                $returned = curl_exec($ch);
+                curl_close ($ch);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$COTW_CHAMPWEEK_URL);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_POST, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    $returned = curl_exec($ch);
-    curl_close ($ch);
-
-    $chweek = json_decode($returned);
-    $chweek = $chweek->champions;
-
-    foreach ($chweek as $COTW_CHAMPIONS) {
-        foreach ($chlist as $COTW_IMAGES) {
-            if($COTW_CHAMPIONS->name == $COTW_IMAGES->name)
+                // API JSON DECODE
+                $chweek = json_decode($returned);
+                $chweek = $chweek->champions;
+                wp_cache_set( 'COWT_CHAMPOFWEEK', $chweek, "COTW", 3600);
+            } 
+        } else {
+            global $COTW_CACHE;
+            // Get Champions Of The Week
+            // Verify if data are cached
+            if(time() - fileatime($COTW_CACHE) > 3600)
             {
-                $x = $COTW_IMAGES->image->x*-1;
-                $y = $COTW_IMAGES->image->y*-1;
-                echo "<div style='float:left;width:100;height:70;'>" . $COTW_IMAGES->name." : <div style=\"display:block;height:48;width:48;background-image:url('". $COTW_SPRITEDIR_URI . $COTW_IMAGES->image->sprite ."');background-position:left " . $x . " top " . $y ." ;\"></div></div>";
+                // API REQUEST
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,$COTW_CHAMPWEEK_URL);
+                curl_setopt($ch, CURLOPT_VERBOSE, 1);
+                curl_setopt($ch, CURLOPT_POST, 0);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                $returned = curl_exec($ch);
+                curl_close ($ch);
+
+                // API JSON DECODE
+                $chweek = json_decode($returned);
+                $chweek = $chweek->champions;
+
+                file_put_contents($COTW_CACHE, serialize($chweek));
+            } else {
+                $chweek = unserialize(file_get_contents($COTW_CACHE));
             }
-        }  
+        }
+        // Return Champions Array
+        return $chweek;
+    }
+
+    function COTW_Init()
+    {
+        global $COTW_CHAMPLIST;
+        global $COTW_CHAMPWEEK;
+
+        $COTW_CHAMPLIST = COTW_GetChampionsList();
+        $COTW_CHAMPWEEK = COTW_GetChampionsOfTheWeek();
+    }
+
+    COTW_Init();
+    
+    function COTW_Show()
+    {
+        global $COTW_CHAMPLIST;
+        global $COTW_CHAMPWEEK;
+        global $COTW_SPRITEDIR_URI;
+
+        foreach ($COTW_CHAMPWEEK as $COTW_CHAMPIONS) {
+            foreach ($COTW_CHAMPLIST as $COTW_IMAGES) {
+                if($COTW_CHAMPIONS->name == $COTW_IMAGES->name)
+                {
+                    $x = $COTW_IMAGES->image->x*-1;
+                    $y = $COTW_IMAGES->image->y*-1;
+                    echo "<div style='float:left;width:100;height:70;'>" . $COTW_IMAGES->name." : <div style=\"display:block;height:48;width:48;background-image:url('". $COTW_SPRITEDIR_URI . $COTW_IMAGES->image->sprite ."');background-position:left " . $x . "px top " . $y ."px;\"></div></div>";
+                }
+            }  
+        }
     }
 ?>
